6df087598f9915d2d7f813c6c16b6aca3c99a459
Giga <gigakatamadze0@gmail.com>
2024-04-26 18:42:25 +0400

    Set up a php debugger for the project

--- public/index.php
+++ public/index.php
@@ -1,7 +1,19 @@
 <?php
-require "../helpers.php";
+// phpinfo();
 
+require "../helpers.php";
+// xdebug_info();
+
+require basePath("Router.php");
+
+// xdebug_info();
+
+$router = new Router();
+
+
+$routes = require basePath("routes.php");
 
 $uri = $_SERVER["REQUEST_URI"];
+$method = $_SERVER["REQUEST_METHOD"];
 
-require basePath("router.php");
+$router->route($uri, $method);
