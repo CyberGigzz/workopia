c15f3248c6c35c8f25e13419b236667b6f18ce64
Giga <gigakatamadze0@gmail.com>
2024-05-19 13:02:55 +0400

    Basic php crud application is ready

--- App/views/listings/show.view.php
+++ App/views/listings/show.view.php
@@ -5,13 +5,15 @@
 
 <section class="container mx-auto p-4 mt-4">
     <div class="rounded-lg shadow-md bg-white p-3">
+        <?= loadPartial('message') ?>
         <div class="flex justify-between items-center">
             <a class="block p-4 text-blue-700" href="/listings">
                 <i class="fa fa-arrow-alt-circle-left"></i>
                 Back To Listings
             </a>
             <div class="flex space-x-4 ml-4">
-                <a href="/edit" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
+                <a href="/listings/edit/<?= $listing->id ?>"
+                    class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
                 <!-- Delete Form -->
                 <form method="POST">
                     <input type="hidden" name="_method" value="DELETE">
@@ -23,7 +25,7 @@
         </div>
         <div class="p-4">
             <h2 class="text-xl font-semibold"><?= $listing->title ?></h2>
-            <p class="text-gray-700 text-lg mt-2">
+            <p class=" text-gray-700 text-lg mt-2">
                 <?= $listing->description ?>
             </p>
             <ul class="my-4 bg-gray-100 p-4">
